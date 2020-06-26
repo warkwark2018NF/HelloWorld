f8accee139b8fc93adb3ce447b5882f2916eb41b
MasakiFusama <fusama_m@me.com>
2020-06-26 10:34:44 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by FUSAMAMASAKI on 2020/06/26.
+//  Copyright © 2020 FUSAMAMASAKI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
